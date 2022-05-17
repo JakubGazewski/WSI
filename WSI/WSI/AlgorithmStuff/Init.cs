a9d8a354bcdba3be438491ec1607a299ddd0e574
adamWodz <01151602@pw.edu.pl>
2022-05-17 23:14:20 +0200

    added init chromosomes and wrong move reactions

---
+++ WSI/WSI/AlgorithmStuff/Init.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSI.AlgorithmStuff
+{
+    class Init
+    {
+        private int _boardWidth; 
+        private int _boardHeight; 
+        private int _emptyTileX; 
+        private int _emptyTileY;
+
+
+        public Init(int boardWidth, int boardHeight, int emptyTileX = 0, int emptyTileY = 0)
+        {
+            _boardWidth = boardWidth;
+            _boardHeight = boardHeight;
+            _emptyTileX = emptyTileX;
+            _emptyTileY = emptyTileY;
+        }
+
+
+        public IList<StringBuilder> GetStartngChromosomes(int chromosomesNumber, int chromosomesLength)
+        {
+            IList <StringBuilder> chromosomes = new List<StringBuilder>();
+            Mutation mutation = new Mutation();
+
+            for (int i = 0; i < chromosomesNumber; i++)
+            {
+                StringBuilder chromosome = new StringBuilder();
+                for(int j = 0; j < chromosomesLength; j++)
+                {
+                    bool canGoOn = mutation.wrongMoveReaction == WrongMoveReaction.ignore ? true : false;
+                    do
+                    {
+                        mutation.AddGene(ref chromosome);
+                        if (mutation.Check(chromosome, _boardWidth, _boardHeight, _emptyTileX, _emptyTileY)) canGoOn = true;
+                    } while (!canGoOn);
+                }
+                chromosomes.Add(chromosome);
+            }
+
+            return chromosomes;
+        }
+    }
+}
--- WSI/WSI/AlgorithmStuff/Mutation.cs
+++ WSI/WSI/AlgorithmStuff/Mutation.cs
@@ -7,42 +7,36 @@ using WSI.UI_stuff;
 
 namespace WSI.AlgorithmStuff
 {
-    public enum Allel
-    {
-        U = 'U',
-        D = 'D',
-        R = 'R',
-        L = 'L'
-    }
-
+    
     class Mutation
     {
         readonly Array moves = Enum.GetValues(typeof(Allel));
         readonly Random random = new();
+        public readonly WrongMoveReaction wrongMoveReaction = WrongMoveReaction.ignore;
 
-        public void AddGene(ref string chromosome)
+        public void AddGene(ref StringBuilder chromosome)
         {
             Allel randomMove = (Allel)moves.GetValue(random.Next(moves.Length));
-            chromosome += (char)randomMove;
+            chromosome.Append((char)randomMove);
         }
 
-        public void MutateGene(ref string chromosome)
+        public void MutateGene(ref StringBuilder chromosome)
         {
             int randomIndex = random.Next(chromosome.Length);
             //Console.WriteLine(randomIndex);
             Allel randomMove = (Allel)moves.GetValue(random.Next(moves.Length));
             //Console.WriteLine(randomMove);
-            StringBuilder sb = new(chromosome);
-            sb[randomIndex] = (char)randomMove;
-            chromosome = sb.ToString();
+            chromosome[randomIndex] = (char)randomMove;
         }
 
-        public bool Check(string chromosome, int boardWidth, int boardHeight, int emptyTileX, int emptyTileY) //sprawdzenie czy nie ma ruchu poza planszę w wyniku mutacji
+        public bool Check(StringBuilder chromosome, int boardWidth, int boardHeight, int emptyTileX, int emptyTileY) //sprawdzenie czy nie ma ruchu poza planszę w wyniku mutacji
         {
             int width = emptyTileY, height = emptyTileX;
 
-            foreach (char c in chromosome)
+            //foreach (char c in chromosome)
+            for(int i = 0; i < chromosome.Length; i++)
             {
+                char c = chromosome[i];
                 switch (c)
                 {
                     case 'U':
@@ -65,26 +59,27 @@ namespace WSI.AlgorithmStuff
             return true;
         }
 
-        public void Mutate(ref string chromosome, int mutate_chance, int add_vs_mutate_chance, int boardWidth, int boardHeight, int emptyTileX = 0, int emptyTileY = 0) //wartości mutate_chance i add_vs_mutate_chance od 0 do 100
+        //wartości mutate_chance i add_vs_mutate_chance od 0 do 100
+        public void Mutate(ref StringBuilder chromosome, int mutate_chance, int add_vs_mutate_chance, int boardWidth, int boardHeight, int emptyTileX = 0, int emptyTileY = 0) 
         {
-            int chance = random.Next(100);
+            int mutationDraw = random.Next(100);
             //Console.WriteLine(chance);
-            if (chance < mutate_chance)
+            if (mutationDraw < mutate_chance)
             {
-                int chance2 = random.Next(100);
+                int addDraw = random.Next(100);
                 //Console.WriteLine(chance2);
-                string newChromosome = chromosome;
+                StringBuilder newChromosome = chromosome;
 
-                if (chance2 < add_vs_mutate_chance)
-                    AddGene(ref newChromosome);
-                else
-                    MutateGene(ref newChromosome);
-                //Console.WriteLine($"newChromosome:{newChromosome}");
-                if (Check(newChromosome, boardWidth, boardHeight, emptyTileX, emptyTileY))
+                bool canGoOn = wrongMoveReaction == WrongMoveReaction.ignore ? true : false;
+                do
                 {
-                    chromosome = newChromosome;
-                    //Console.WriteLine($"chromosome:{chromosome}");
-                }
+                    if (addDraw < add_vs_mutate_chance)
+                        AddGene(ref newChromosome);
+                    else
+                        MutateGene(ref newChromosome);
+                    if(Check(newChromosome, boardWidth, boardHeight, emptyTileX, emptyTileY)) canGoOn = true;
+                } while (!canGoOn);
+
             }
         }
     }
